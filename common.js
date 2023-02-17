fab69b366a6aeadace353c3854a77c3ec763af4a
kumhyunjin <jinieguk97@gmail.com>
2023-02-17 17:34:55 +0900

    1차수정/2.17

--- common.js
+++ common.js
@@ -1,7 +1,26 @@
-var swiper = new Swiper(".news_recent", {
+// swiper (Responsive breakpoints)
+
+let newsRecent = new Swiper(".news_recent", {
   navigation: {
     nextEl: ".next1",
     prevEl: ".prev1",
   },
-  slidesPerView: 5,
+  slidesPerView: 1,
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1100: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+// 햄버거 버튼
+const $ham = document.querySelector(".ham");
+const $topNav = document.querySelector(".top_nav");
+$ham.addEventListener("click", function () {
+  $topNav.classList.toggle("on");
 });
